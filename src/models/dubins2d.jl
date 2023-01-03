19b7c2ec552d19556940646dd69cfdbc30355080
Kei18 <keisuke.oku18@gmail.com>
2023-01-03 12:08:50 +0900

    add distance limitation

--- src/models/dubins2d.jl
+++ src/models/dubins2d.jl
@@ -65,8 +65,8 @@ function gen_connect(
     q::StateDubins,  # to identify type
     obstacles::Vector{CircleObstacle2D},
     rads::Vector{Float64};
-    step_time::Float64 = STEP_TIME,
-    max_dist::Union{Nothing,Float64} = nothing,
+    step_time::Float64=STEP_TIME,
+    max_step_dist::Float64=sqrt(3) / 4
 )::Function
 
     n_dividing = Int(floor(1 / step_time))
@@ -83,10 +83,10 @@ function gen_connect(
     end
 
     f(q_from::StateDubins, q_to::StateDubins, i::Int64)::Bool = begin
-        D = dist(q_from, q_to)
-        !isnothing(max_dist) && D > max_dist && return false
+        # check \delta
+        dist(q_from, q_to) > max_step_dist && return false
 
-        P = get_dubins_points(q_from, q_to, rads[i]; n_dividing = n_dividing)
+        P = get_dubins_points(q_from, q_to, rads[i]; n_dividing=n_dividing)
         isnothing(P) && return false
         for p in P
             # outside
--- src/models/point2d.jl
+++ src/models/point2d.jl
@@ -40,7 +40,8 @@ end
 function gen_connect(
     q::StatePoint2D,  # to identify type
     obstacles::Vector{CircleObstacle2D},
-    rads::Vector{Float64},
+    rads::Vector{Float64};
+    max_step_dist::Float64 = sqrt(2)/4,
 )::Function
 
     # check: q \in C_free
@@ -55,6 +56,9 @@ function gen_connect(
     end
 
     f(q_from::StatePoint2D, q_to::StatePoint2D, i::Int64)::Bool = begin
+        # check \delta
+        dist(q_from, q_to) > max_step_dist && return false
+
         any(x -> (x < rads[i] || 1 - rads[i] < x), [q_to.x, q_to.y]) && return false
 
         # check: collisions with static obstacles
